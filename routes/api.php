d078a07bfe4e5d4cdb4bcd6b53ad3369680a8aa2
Alex Christian <aquispe.developer@gmail.com>
2018-03-31 13:58:08 -0500

    creado archivo json para realizar pruebas logicas  en base a una simulacion de active directory

--- routes/api.php
+++ routes/api.php
@@ -13,23 +13,36 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(['middleware' => ['cors']], function () {
-    if (!request()->ajax()) {
-        Route::get('/', function () {
-            return response()->view("errors.404");
-        });
-    }
-});
+//Route::group(['middleware' => ['cors']], function () {
+//    if (!request()->ajax()) {
+//        Route::get('/', function () {
+//            return response()->view("errors.404");
+//        });
+//    }
+//});
 
 Route::group(['middleware' => ['cors:api']], function () {
 
-    if (!request()->ajax()) {
-        Route::get('/{any}', function () {
-            return response()->view("errors.404");
-        });
-    } else {
+//    if (!request()->ajax()) {
+//        Route::get('/{any}', function () {
+//            return response()->view("errors.404");
+//        });
+//    } else {
         Route::get("/generate-token", "FirebaseController@firebaseGenerate");
         Route::get("/get-config", "Controller@configProjectGeneral");
+        Route::get("/unlockresetuser/search/{textSearch}",function($textSearch){
+                $string = file_get_contents(asset("/data.json"));
+                $array = json_decode($string);
+                foreach ($array as $item){
+                    $item = (object)$item;
+                    if($textSearch == $item->username || $textSearch == $item->dni || $textSearch == $item->email){
+                        return response()->json($item,200);
+                    } else {
+                        continue;
+                    }
+                }
+                return response()->json("No se ha encontrado a este usuario",412);
+        });
 
         Route::group(['middleware' => ['verify.headers:api']], function () {
             //Unlock
@@ -39,6 +52,6 @@ Route::group(['middleware' => ['cors:api']], function () {
             //Search
             Route::post("/create-log-search", "SearchController@createLog");
         });
-    }
+//    }
 
 });
